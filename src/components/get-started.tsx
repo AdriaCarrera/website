6086dfb31c569bc8367e348854ed6821b296e08b
Longbo <longboqiu@hotmail.com>
2024-09-23 09:05:00 +0200

    feat: add `get-started` section

--- src/components/get-started.tsx
+++ src/components/get-started.tsx
@@ -22,36 +22,36 @@ function ExternalLink({ href, label }: ExternalLinkProps) {
 export function GetStarted(): JSX.Element {
   return (
     <section className="w-full">
-      <div className="px-4 py-8 pt-[100px] md:pl-20 md:pb-16 bg-background">
+      <div className="px-4 py-8 md:pt-[100px] md:pl-20 md:pb-16 bg-background">
         <p className="font-bold text-xl md:text-[40px]">Get started on the XRPL EVM Sidechain</p>
       </div>
 
-      <div className="relative h-[776px] md:h-auto md:aspect-[2/1] overflow-hidden">
+      <div className="relative h-[396px] md:h-auto md:aspect-[2/1] overflow-hidden">
         <div className="absolute inset-0 bg-gradient-to-r from-blue-500 to-black"></div>
 
         <div className="absolute inset-0 grid-overlay">
-          <div className="grid grid-cols-[repeat(4,1fr)_0.5fr] grid-rows-[repeat(5,1fr)_0.5fr] md:grid-cols-16 md:grid-rows-8 h-full w-full">
-            <div className="ml-4 md:mt-0 md:ml-0 md:my-0 h-[50px] md:h-auto col-span-4 col-start-1 row-start-1 md:col-start-3 md:row-start-2 md:col-span-4 md:row-span-1 bg-[#232325] border-[0.5px] border-white p-4 md:p-5">
+          <div className="grid grid-cols-[repeat(4,1fr)_0.5fr] grid-rows-[repeat(5,1fr)_0.5fr] md:grid-cols-16 md:grid-rows-8 h-full w-full gap-4 md:gap-0">
+            <div className="ml-4 mt-4 md:m-0 h-[50px] md:h-auto col-span-4 col-start-1 row-start-1 md:col-start-3 md:row-start-2 md:col-span-4 md:row-span-1 bg-[#232325] border-[0.5px] border-white p-4 md:p-5">
               <ExternalLink href="" label="Build an XRPL cross-chain dApp" />
             </div>
 
-            <div className="ml-4 md:mt-0 md:ml-0 md:my-0 h-[50px] md:h-auto col-span-4 col-start-1 row-start-2 md:col-start-3 md:row-start-3 md:col-span-4 md:row-span-1 bg-[#232325] border-[0.5px] border-white p-4 md:p-5">
+            <div className="ml-4 mt-[64px] md:m-0 h-[50px] md:h-auto col-span-4 col-start-1 row-start-1 md:col-start-3 md:row-start-3 md:col-span-4 md:row-span-1 bg-[#232325] border-[0.5px] border-white p-4 md:p-5">
               <ExternalLink href="" label="Build an EVM-only dApp" />
             </div>
 
-            <div className="ml-4 md:mt-0 md:ml-0 md:my-0 h-[50px] md:h-auto col-span-4 col-start-1 row-start-3 md:col-start-7 md:row-start-4 md:col-span-4 md:row-span-1 bg-[#232325] border-[0.5px] border-white p-4 md:p-5">
+            <div className="ml-4 md:m-0 h-[50px] md:h-auto col-span-4 col-start-1 row-start-2 md:col-start-7 md:row-start-4 md:col-span-4 md:row-span-1 bg-[#232325] border-[0.5px] border-white p-4 md:p-5">
               <ExternalLink href="" label="Select a wallet" />
             </div>
 
-            <div className="ml-4 md:mt-0 md:ml-0 md:my-0 h-[50px] md:h-auto col-span-4 col-start-1 row-start-4 md:col-start-7 md:row-start-5 md:col-span-4 md:row-span-1 bg-[#232325] border-[0.5px] border-white p-4 md:p-5">
+            <div className="ml-4 mt-[50px] md:m-0 h-[50px] md:h-auto col-span-4 col-start-1 row-start-2 md:col-start-7 md:row-start-5 md:col-span-4 md:row-span-1 bg-[#232325] border-[0.5px] border-white p-4 md:p-5">
               <ExternalLink href="" label="Try a dApp" />
             </div>
 
-            <div className="ml-4 md:mt-0 md:ml-0 md:my-0 h-[50px] md:h-auto col-span-4 col-start-1 row-start-5 md:col-start-11 md:row-start-5 md:col-span-4 md:row-span-1 bg-[#232325] border-[0.5px] border-white p-4 md:p-5">
+            <div className="ml-4 md:m-0 h-[50px] md:h-auto col-span-4 col-start-1 row-start-3 md:col-start-11 md:row-start-5 md:col-span-4 md:row-span-1 bg-[#232325] border-[0.5px] border-white p-4 md:p-5">
               <ExternalLink href="" label="Explorer Grants" />
             </div>
 
-            <div className="ml-4 md:mt-0 md:ml-0 md:my-0 h-[50px] md:h-auto col-span-4 col-start-1 row-start-6 md:col-start-11 md:row-start-6 md:col-span-4 md:row-span-1 bg-[#232325] border-[0.5px] border-white p-4 md:p-5">
+            <div className="ml-4 mt-[50px] md:m-0 h-[50px] md:h-auto col-span-4 col-start-1 row-start-3 md:col-start-11 md:row-start-6 md:col-span-4 md:row-span-1 bg-[#232325] border-[0.5px] border-white p-4 md:p-5">
               <ExternalLink href="" label="Join the community" />
             </div>
 
