23c1af47d3646b8804117160217876cbdf148254
Longbo <longboqiu@hotmail.com>
2024-10-02 11:21:05 +0200

    feat: add links to `builders` section

--- src/components/builders.tsx
+++ src/components/builders.tsx
@@ -1,17 +1,23 @@
+import Link from "next/link";
 import { Icons } from "./icons";
 
 type BuilderProps = {
   name: string;
   description: string;
   logo: JSX.Element;
+  link: string;
 };
 
-function Builder({ description, logo, name }: BuilderProps): JSX.Element {
+function Builder({ description, logo, name, link }: BuilderProps): JSX.Element {
   return (
     <div className="flex flex-col gap-6 items-center">
-      <div className="rounded-full border-[0.75px] border-white h-[118px] w-[118px] md:h-[200px] md:w-[200px] flex items-center justify-center">
+      <Link
+        href={link}
+        target="_blank"
+        className="rounded-full border-[0.75px] border-white h-[118px] w-[118px] md:h-[200px] md:w-[200px] flex items-center justify-center hover:opacity-80"
+      >
         {logo}
-      </div>
+      </Link>
       <div className="flex flex-col text-center">
         <p className="text-xs md:text-xl font-bold">{name}</p>
         <p className="text-xs md:text-xl text-[#838386]">{description}</p>
@@ -25,26 +31,31 @@ const builders: BuilderProps[] = [
     name: "Peersyst",
     description: "Primary innovator and XRPL EVM Sidechain developer",
     logo: <Icons.PeersystLogo className="w-20 md:w-auto" />,
+    link: "https://peersyst.com/",
   },
   {
     name: "Ripple",
     description: "Co-creator of the EVM Sidechain and one of many developers contributing to the XRP Ledger",
     logo: <Icons.RippleLogo className="w-20 md:w-auto" />,
+    link: "https://ripple.com/",
   },
   {
     name: "Evmos",
     description: "Provides Evmos SDK that brings EVM compatibility on top of Cosmos client ",
     logo: <Icons.EvmosLogo className="w-20 md:w-auto" />,
+    link: "https://evmos.org/",
   },
   {
     name: "Axelar",
     description: "Connecting XRP Ledger to EVM world via bridge secured by Axelar network validators",
     logo: <Icons.AxelarLogo className="w-20 md:w-auto" />,
+    link: "https://www.axelar.network/",
   },
   {
     name: "Blockscout",
     description: "Building EVM block explorer",
     logo: <Icons.BlockscoutLogo className="w-20 md:w-auto" />,
+    link: "https://www.blockscout.com/",
   },
 ];
 
