9f430d06f1cff7be82da7bdba473dfd3584348d7
Longbo <longboqiu@hotmail.com>
2024-10-03 11:14:50 +0200

    feat: add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { workSans } from "@/styles/fonts";
 import { cn } from "@/lib/utils";
 import { TailwindIndicator } from "@/components/tailwind-indicator";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "XRPL EVM Sidechain",
@@ -12,6 +13,15 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-KJN0XF1YMF" strategy="afterInteractive" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-KJN0XF1YMF');
+          `}
+      </Script>
       <body className={cn(workSans.className, "dark")}>
         {children}
         <TailwindIndicator />
